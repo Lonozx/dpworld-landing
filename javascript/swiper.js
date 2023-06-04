b707ca075287710a119603c7499f78f2b691f3eb
Lonozx <lonozx@gmail.com>
2023-06-04 22:50:14 +0300

    updated maps part

---
+++ javascript/swiper.js
@@ -0,0 +1,18 @@
+import Swiper from "swiper";
+
+ let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
